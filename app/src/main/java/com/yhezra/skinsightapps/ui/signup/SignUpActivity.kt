2ce008382557c82b35473752ca6c8d30eb538585
Ridwan Raditya <ridwan.raditya01@gmail.com>
2023-05-10 23:08:35 +0700

    Revert "up login, signup"
    This reverts commit f165bcf24e2510ebef27a7432984e469f368acb2.

--- app/src/main/java/com/yhezra/skinsightapps/ui/signup/SignUpActivity.kt
+++ app/src/main/java/com/yhezra/skinsightapps/ui/signup/SignUpActivity.kt
@@ -3,7 +3,6 @@ package com.yhezra.skinsightapps.ui.signup
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.appcompat.app.AlertDialog
 import com.yhezra.skinsightapps.databinding.ActivitySignUpBinding
 import com.yhezra.skinsightapps.ui.MainMenuActivity
 import com.yhezra.skinsightapps.ui.login.LoginActivity
@@ -19,41 +18,10 @@ class SignUpActivity : AppCompatActivity() {
             val moveToSignInActivity = Intent(this@SignUpActivity, LoginActivity::class.java)
             startActivity(moveToSignInActivity)
         }
-
-        confInput()
-    }
-
-    private fun confInput() {
         binding.signupButton.setOnClickListener {
-            val name = binding.nameEditText.text.toString()
-            val email = binding.emailEditText.text.toString()
-            val password = binding.passwordEditText.text.toString()
-            when {
-                name.isEmpty() -> {
-                    binding.nameEditTextLayout.error = "Masukkan nama"
-                }
-                email.isEmpty() -> {
-                    binding.emailEditTextLayout.error = "Masukkan email"
-                }
-                password.isEmpty() -> {
-                    binding.passwordEditTextLayout.error = "Masukkan password"
-                }
-                else -> {
-                    val moveToSignInActivity = Intent(this@SignUpActivity, MainMenuActivity::class.java)
-                    startActivity(moveToSignInActivity)
-                    finish()
-//                    signupViewModel.saveUser(UserModel(name, email, password, false))
-//                    AlertDialog.Builder(this).apply {
-//                        setTitle("Berhasil!")
-//                        setMessage("Akun sudah berhasil dibuat sudah jadi nih. Yuk, login dan belajar coding.")
-//                        setPositiveButton("Lanjut") { _, _ ->
-//                            finish()
-//                        }
-//                        create()
-//                        show()
-//                    }
-                }
-            }
+            val moveToSignInActivity = Intent(this@SignUpActivity, MainMenuActivity::class.java)
+            startActivity(moveToSignInActivity)
         }
+
     }
 }
