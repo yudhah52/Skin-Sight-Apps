b1a0827693fb986089f23bd19585bee6974b1316
Yudha <yudhah52@gmail.com>
2023-05-17 16:29:10 +0700

    up onboard 2.0

--- app/src/main/java/com/yhezra/skinsightapps/ui/onboard/OnboardingActivity.kt
+++ app/src/main/java/com/yhezra/skinsightapps/ui/onboard/OnboardingActivity.kt
@@ -3,27 +3,33 @@ package com.yhezra.skinsightapps.ui.onboard
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.viewpager2.widget.ViewPager2
 import com.yhezra.skinsightapps.R
 import com.yhezra.skinsightapps.data.local.model.Onboard
 import com.yhezra.skinsightapps.databinding.ActivityOnboardingBinding
 import com.yhezra.skinsightapps.ui.onboard.adapter.OnboardListAdapter
+import com.yhezra.skinsightapps.ui.onboard.adapter.OnboardingPageChangeCallback
 import com.yhezra.skinsightapps.ui.signup.SignUpActivity
 
 class OnboardingActivity : AppCompatActivity() {
     private lateinit var binding: ActivityOnboardingBinding
-    private lateinit var onboardListAdapter:OnboardListAdapter
+    private lateinit var onboardListAdapter: OnboardListAdapter
 
-    val onboardList:List<Onboard> = listOf(
+    private lateinit var onboardingPageChangeCallback: OnboardingPageChangeCallback
+
+    private var currentPosition = 0
+    private val onboardList: List<Onboard> = listOf(
         Onboard(
             img = R.drawable.img_onboard,
             description = "Selamat datang di aplikasi SkinSight! Ayo lihat lebih jauh seputar kulit kamu!"
         ),
         Onboard(
-            img = R.drawable.img_onboard,
+            img = R.drawable.img_onboard_2,
             description = "Ketahui kondisi kulit dan tipe kulit kamu dalam sekejap!"
         ),
         Onboard(
-            img = R.drawable.img_onboard,
+            img = R.drawable.img_onboard_3,
             description = "Dapatkan kulit sehat yang kamu inginkan, coba sekarang dan lihat hasilnya!"
         ),
     )
@@ -33,16 +39,51 @@ class OnboardingActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        onboardingPageChangeCallback = OnboardingPageChangeCallback(
+            viewPager = binding.viewpagerOnboard,
+            btnLeft = binding.btnLeftOnboard,
+            btnRight = binding.btnRightOnboard,
+            pageSize = onboardList.size,
+        )
         setOnboardList()
 
-        binding.btnOnboard.setOnClickListener {
-            val moveToSignInActivity = Intent(this@OnboardingActivity, SignUpActivity::class.java)
-            startActivity(moveToSignInActivity)
+        binding.apply {
+            btnOnboard.setOnClickListener {
+                val moveToSignInActivity =
+                    Intent(this@OnboardingActivity, SignUpActivity::class.java)
+                startActivity(moveToSignInActivity)
+            }
+            btnRightOnboard.setOnClickListener {
+                if (currentPosition < onboardList.size - 1)
+                    currentPosition += 1
+                setButtonVisibility()
+                onboardingPageChangeCallback.setCurrentItem(currentPosition)
+            }
+            btnLeftOnboard.setOnClickListener {
+                if (currentPosition > 0)
+                    currentPosition -= 1
+                setButtonVisibility()
+                onboardingPageChangeCallback.setCurrentItem(currentPosition)
+
+            }
+        }
+    }
+
+    private fun setButtonVisibility() {
+        if (currentPosition == onboardList.size - 1)
+            binding.btnRightOnboard.visibility = View.GONE
+        else if (currentPosition == 0)
+            binding.btnLeftOnboard.visibility = View.GONE
+        else {
+            binding.btnLeftOnboard.visibility = View.VISIBLE
+            binding.btnRightOnboard.visibility = View.VISIBLE
         }
     }
 
     private fun setOnboardList() {
         onboardListAdapter = OnboardListAdapter(onboardList)
         binding.viewpagerOnboard.adapter = onboardListAdapter
+        binding.viewpagerOnboard.registerOnPageChangeCallback(onboardingPageChangeCallback)
+        currentPosition = onboardingPageChangeCallback.getSelectedPosition()
     }
 }
---
+++ app/src/main/java/com/yhezra/skinsightapps/ui/onboard/adapter/OnboardingPageChangeCallback.kt
@@ -0,0 +1,46 @@
+package com.yhezra.skinsightapps.ui.onboard.adapter
+
+import android.view.View
+import android.widget.ImageView
+import androidx.viewpager2.widget.ViewPager2
+
+class OnboardingPageChangeCallback(
+    private val viewPager: ViewPager2,
+    private val btnLeft: ImageView,
+    private val btnRight: ImageView,
+    private val pageSize:Int,
+) : ViewPager2.OnPageChangeCallback() {
+    private var selectedPosition = 0
+
+
+    override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
+        setButtonVisibility()
+    }
+
+    override fun onPageSelected(position: Int) {
+        selectedPosition = position
+    }
+
+    override fun onPageScrollStateChanged(state: Int) {
+        // Tidak perlu implementasi
+    }
+
+    fun getSelectedPosition(): Int {
+        return selectedPosition
+    }
+
+    fun setCurrentItem(position: Int) {
+        viewPager.setCurrentItem(position, true)
+    }
+
+    private fun setButtonVisibility() {
+        if (selectedPosition == pageSize - 1)
+            btnRight.visibility = View.GONE
+        else if (selectedPosition == 0)
+           btnLeft.visibility = View.GONE
+        else {
+            btnLeft.visibility = View.VISIBLE
+            btnRight.visibility = View.VISIBLE
+        }
+    }
+}
