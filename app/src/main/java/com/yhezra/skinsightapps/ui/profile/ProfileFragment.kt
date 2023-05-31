61d4768240d9ecd13a78fbe4e340707d4eafad01
Yudha <yudhah52@gmail.com>
2023-05-31 16:12:45 +0700

    up logout

--- app/src/main/java/com/yhezra/skinsightapps/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/yhezra/skinsightapps/ui/profile/ProfileFragment.kt
@@ -1,35 +1,87 @@
 package com.yhezra.skinsightapps.ui.profile
 
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.preferencesDataStore
 import androidx.fragment.app.viewModels
 import com.yhezra.skinsightapps.R
+import com.yhezra.skinsightapps.databinding.FragmentProfileBinding
 import com.yhezra.skinsightapps.ui.auth.AuthViewModel
 import com.yhezra.skinsightapps.ui.auth.AuthViewModelFactory
+import com.yhezra.skinsightapps.data.local.Result
+import com.yhezra.skinsightapps.ui.signup.SignUpActivity
 
 class ProfileFragment : Fragment() {
 
-//    private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "auth")
-//
-//    private val authViewModel: AuthViewModel by viewModels {
-//        AuthViewModelFactory.getInstance(dataStore)
-//    }
+    private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "auth")
 
+    private val authViewModel: AuthViewModel by viewModels {
+        AuthViewModelFactory.getInstance(requireContext().dataStore)
+    }
+
+    private var _binding: FragmentProfileBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        authViewModel.isLogin().observe(requireActivity()) {
+            if (it.isNullOrEmpty()) {
+                navigateToSignup()
+            }
+        }
+
+        setupAction()
+    }
+
+    private fun navigateToSignup() {
+        Toast.makeText(
+            requireContext(),
+            getString(R.string.logout),
+            Toast.LENGTH_SHORT
+        ).show()
+        startActivity(Intent(requireActivity(), SignUpActivity::class.java))
+        activity?.finish()
+    }
+
+
+    private fun setupAction() {
+        binding.apply {
+            btnLogout.setOnClickListener {
+                authViewModel.logout().observe(requireActivity()) { result ->
+                    when (result) {
+                        is Result.Loading -> {
+                            binding.progressBar.visibility = View.VISIBLE
+                        }
+                        is Result.Success -> {
+                            binding.progressBar.visibility = View.GONE
+                        }
+                        is Result.Error -> {
+                            binding.progressBar.visibility = View.GONE
+                            Toast.makeText(requireActivity(), "Gagal logout", Toast.LENGTH_SHORT)
+                                .show()
+                        }
+                    }
+                }
+            }
+        }
     }
 
 
